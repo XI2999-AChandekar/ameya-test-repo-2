9b81852bba9add380903ded8d2fc9511931f2117
xlr8s <xlr8s@xebia.com>
2022-12-09 10:41:19 +0000

    added my-eks-2-3759a8f resources

---
+++ envs/dev-4384c0f/01-infra/my-eks-2-3759a8f/03-eks/backend.tf
@@ -0,0 +1,9 @@
+
+terraform {
+  backend "s3" {	
+    bucket = "xlr8s-artifacts"
+    encrypt = "false"
+    key = "ameya-project-12ad70e/envs/dev-4384c0f/01-infra/my-eks-2-3759a8f/03-eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
