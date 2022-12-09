c144d9458f831693de0157c014109b90ea595d78
xlr8s <xlr8s@xebia.com>
2022-12-09 09:03:10 +0000

    added resource-55-3842992 resources

---
+++ envs/dev-4384c0f/01-infra/resource-55-3842992/03-eks/backend.tf
@@ -0,0 +1,9 @@
+
+terraform {
+  backend "s3" {	
+    bucket = "xlr8s-artifacts"
+    encrypt = "false"
+    key = "ameya-project-12ad70e/envs/dev-4384c0f/01-infra/resource-55-3842992/03-eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
