298e1f344f83ee4eb8f41fc058ee7243562bc06d
xlr8s <xlr8s@xebia.com>
2022-12-16 10:21:04 +0000

    added my-eks-dummy-2-371e475 resources

---
+++ envs/dev-4384c0f/01-infra/my-eks-dummy-2-371e475/01-vpc-for-eks/backend.tf
@@ -0,0 +1,9 @@
+
+terraform {
+  backend "s3" {	
+    bucket = "xlr8s-artifacts"
+    encrypt = "false"
+    key = "ameya-project-12ad70e/envs/dev-4384c0f/01-infra/my-eks-dummy-2-371e475/01-vpc-for-eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
