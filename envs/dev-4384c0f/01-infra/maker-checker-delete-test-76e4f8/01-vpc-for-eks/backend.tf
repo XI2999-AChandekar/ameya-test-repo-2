b7a1c382f54ced2f8085a96466f6396bf9b36289
xlr8s <xlr8s@xebia.com>
2022-12-22 10:03:28 +0000

    added maker-checker-delete-test-76e4f8 resources

---
+++ envs/dev-4384c0f/01-infra/maker-checker-delete-test-76e4f8/01-vpc-for-eks/backend.tf
@@ -0,0 +1,9 @@
+
+terraform {
+  backend "s3" {	
+    bucket = "xlr8s-artifacts"
+    encrypt = "false"
+    key = "ameya-project-12ad70e/envs/dev-4384c0f/01-infra/maker-checker-delete-test-76e4f8/01-vpc-for-eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
